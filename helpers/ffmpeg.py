c2196680bf9fb5d944d87851915e54bbbe8bd2b3
GokuOp7 <153523489+GokuOp7@users.noreply.github.com>
2024-01-27 23:03:06 +0530

    Update ffmpeg.py

--- helpers/ffmpeg.py
+++ helpers/ffmpeg.py
@@ -12,12 +12,12 @@ async def enc_video(client, message, data):
     await message.edit_text(f"`{dwld_loc}`\n\nEncoding Video ...\n\n**please wait**")
 
     out_loc = os.path.splitext(dwld_loc)[0]
-    out_loc = out_loc + "_320p.mp4"
+    out_loc = out_loc + "(480p).mkv"
     
     #out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -map 0:{data['map']} -af \"pan=stereo|c0=c0|c1=c0\" -ar 48000 -ab 256k -f mp3 \"{out_loc}\" -y")
-    out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -c:v libx264 -crf 20 -s 320*240 -c:a aac -ar 48000 -ab 96k \"{out_loc}\" -y")
+    out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -c:v libx265 -preset veryfast -crf 28 -s 854*480 -c:a aac -ab 35k \"{out_loc}\" -y")
     if rcode != 0:
-        await message.edit_text(f"**(320p mp4 96k) - Error Occured.\n\n{err}**")
+        await message.edit_text(f"**(480p mkv 35k) - Error Occured.\n\n{err}**")
         print(err)
         await clean_up(dwld_loc, out_loc)
         return
